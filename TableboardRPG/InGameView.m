79170eff23b5ed275ce6afa747cbc79248d32c71
Mario Toledo <mariotoledo12@gmail.com>
2012-03-07 17:56:17 -0300

    first commit

---
+++ TableboardRPG/InGameView.m
@@ -0,0 +1,31 @@
+//
+//  InGameView.m
+//  TableboardRPG
+//
+//  Created by Mario Leandro Pires Toledo on 3/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "InGameView.h"
+
+@implementation InGameView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
